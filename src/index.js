8d8c51102b8a7c4440fcbf63f177cd42016d7c75
Martin Jonsson <51284952+arnell0@users.noreply.github.com>
2023-06-28 07:40:41 +0000

    published to npm and cleaned up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Table from './just-table';
+
+function Example() {
+
+  const data = Array.from(Array(10000).keys()).map(item => ({
+    id: item,
+    first_name: 'John',
+    last_name: 'Doe',
+    age: 30,
+    random: Math.round(Math.random() * 10000),
+  }))
+
+  const columns = [
+    'Id',
+    'First Name',
+    'Last Name',
+    'Age',
+  ]
+
+  return (
+    <div>
+      <Table 
+        data={data}
+        autoGenerateColumns={true}
+        // columns={columns}
+        pagination={true}
+        paginationSize={10}
+      />
+    </div>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>
 );
 
